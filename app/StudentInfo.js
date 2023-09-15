64434bbd2adc3c7172845ed58bdcc39f300c0973
TygerNarong <curtis.narong@edu.sait.ca>
2023-09-15 11:46:03 -0600

    FInished Assignment

---
+++ app/StudentInfo.js
@@ -0,0 +1,12 @@
+
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return (
+        <div>
+            <p>Curtis Narong</p>
+            <p>CPRG 306 a</p>
+            <Link href="https://github.com/TygerNarong/webdev2-demos-c">Github Repository</Link>
+        </div>
+    )
+}
