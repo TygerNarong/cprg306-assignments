8b9971ea23a27a471ad27ad42f36a5d9cb5e0d9c
TygerNarong <curtis.narong@edu.sait.ca>
2023-10-02 19:17:53 -0600

    week 3 assignment
    added week 3 assignment

---
+++ app/week3/page.js
@@ -0,0 +1,12 @@
+import ItemList from './item-list.js';
+
+function Page(){
+    return (
+    <main className="bg-gray-100 min-h-screen p-4">
+        <h1 className="text-black text-3xl font-bold mb-4">Shopping List</h1>
+        <ItemList />
+    </main>
+    );
+}
+
+export default Page;
