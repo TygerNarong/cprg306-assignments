c7fab49558aecc5492ef0108c6dd005d7b7c18cc
TygerNarong <curtis.narong@edu.sait.ca>
2023-10-09 21:18:40 -0600

    week4
    week 4 assignment added

--- app/page.js
+++ app/page.js
@@ -7,8 +7,15 @@ export default function Home() {
     <main>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
       <StudentInfo />
+      <div>
       <Link href="/week2">Week 2</Link>
+      </div>
+      <div>
       <Link href="/week3">Week 3</Link>
+      </div>
+      <div>
+      <Link href="/week4">Week 4</Link>
+      </div>
 
     </main>
   )
